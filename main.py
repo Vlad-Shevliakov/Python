fef6f76fbd4c0d8b18f9fe8ec1f6679b39132460
Vlad-Shevliakov <vladislavs319@gmail.com>
2019-03-02 22:20:08 +0200

    ttc

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-import sys
+import random
 
-million = list(range(0, 1000001))
+grid = {
+    'tl': ' ', 'tm': ' ', 'tr': ' ',
+    'ml': ' ', 'mm': ' ', 'mr': ' ',
+    'll': ' ', 'lm': ' ', 'lr': ' '
+}
 
-# min and max value of array
+picked_arr_items = list(range(len(grid)))
 
-print('Mb in memory: {}'.format(sys.getsizeof(million) / 1000000))
+def get_grid():
+    print(grid['tl'] + '|' + grid['tm'] + '|' + grid['tr'])
+    print('-+-+-')
+    print(grid['ml'] + '|' + grid['mm'] + '|' + grid['mr'])
+    print('-+-+-')
+    print(grid['ll'] + '|' + grid['lm'] + '|' + grid['lr'])
+    print('#' * 7)
 
-print('Min value: {}'.format(min(million)))
-print('Max value: {}'.format(max(million)))
+# get_grid()
+
+def random_add(grid, arr):
+    count = 0
+    for key, value in grid.items():
+        if value != ' ':
+            arr[count] = True
+        else:
+            arr[count] = key
+        count += 1
+    
 
 
-print('Sum all of numbers: {}'.format(sum(million)))
-
-arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+for trial in range(5):
+    get_grid()
+    user_coord = input('Add to: ')
+    if user_coord == 'e':
+        break
+    user_item = input('Add to: ')
+    try:
+        if grid[user_coord] == ' ':
+            grid[user_coord] = user_item[0]
+        else:
+            continue
+    except KeyError:
+        print('I need something like: tm, tr, mr...')
+        continue
+    get_grid()
+    random_add(grid, picked_arr_items)
